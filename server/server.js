aee7f5eb6c265272d9b40c5e12c0aeb59c5bee3f
Dv <dipvachhani1111@gmail.com>
2020-07-12 23:45:45 +0530

    chang port to 3001

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.static(publicPath));
 
@@ -11,5 +11,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("Server is up!");
+  console.log("Server is up! on Port: " + port);
 });
